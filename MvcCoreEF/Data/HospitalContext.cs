f94a4090dce721a036bdb1113bd8d5a7ed4d6718
Oscargm13 <oskitargm5@gmail.com>
2025-02-12 22:06:34 +0100

    Add project files.

---
+++ MvcCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Data
+{
+    public class HospitalContext: DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options)
+        {   }
+        public DbSet<Hospital> Hospitales { get; set; }
+
+    }
+}
